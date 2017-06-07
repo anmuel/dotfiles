3cb39e2e040a29d35224ee0a3facfc30f51320c5
Andreas Müller <andreas-b.mueller@rohde-schwarz.com>
2017-06-07 14:56:07 +0200

    Add yarn

--- install-npm-env.sh
+++ install-npm-env.sh
@@ -4,4 +4,4 @@
 # Useful when a new node version is released and gets installed via nvm
 # To ensure a basic global setup
 npm install -g npm npm-check-updates np git-stats tiny-care-terminal \
-  git-standup readme @angular/cli jeopardy nodemon --registry http://registry.npmjs.org
+  git-standup readme @angular/cli jeopardy nodemon yarn --registry http://registry.npmjs.org
